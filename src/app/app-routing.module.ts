3f272354ac42ae6b7dfa12196646197eb6b70e82
Dhaval150 <d.r.chavda1509@gmail.com>
2023-05-15 18:05:39 +0530

    TO-DO_3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'to-do', component: ToDoComponent},
+  {path: '', redirectTo: 'to-do', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
